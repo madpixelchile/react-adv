ce3b943669681a63c6cbd36ec6df5b61df33cf5b
Juan Escudero <juanescudero@MacBook-Pro-de-Juan.local>
2023-09-07 21:41:31 -0300

    First commit with lazyload

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,4 @@
-import { BrowserRouter } from 'react-router-dom';
-import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
+import { Routes, Route, NavLink, Navigate, BrowserRouter } from 'react-router-dom';
 
 import logo from '../logo.svg'
 
@@ -27,7 +26,6 @@ export const Navigation = () => {
                     <Route path="about" element={ <h1>About Page</h1> } />
                     <Route path="users" element={ <h1>Users Page</h1> } />
                     <Route path="home" element={ <h1>Home Page</h1> } />
-                    
                     <Route path="/*" element={ <Navigate to="/home" replace /> } />
                 </Routes>
 
